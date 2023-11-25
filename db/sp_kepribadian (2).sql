75a529e44bc0e41b246bc225f21e72d40b672f5b
Iqbal Pamula Ba'iq <balpamula@gmail.com>
2023-11-25 16:19:45 +0700

    update db

--- db/sp_kepribadian (2).sql
+++ db/sp_kepribadian (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2023 at 08:49 AM
+-- Generation Time: Jul 27, 2023 at 07:03 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.1.12
 
